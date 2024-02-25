c0d7c6e16b9837de5ed818f09d3dddfd9d686db8
Harry <lm22433@bristol.ac.uk>
2024-02-25 23:41:13 +0000

    dynamic number of tokes graph

--- backend/src/vapes/vapes.service.ts
+++ backend/src/vapes/vapes.service.ts
@@ -77,4 +77,47 @@ export class VapesService {
       },
     });
   }
+
+  async getClusteredDailyVapesByUserId(userId: string) {
+    const weeklyVapes = await this.getWeeklyVapesByUserId(userId);
+    return this.clusterIntoDays(weeklyVapes);
+  }
+
+  clusterIntoDays(
+    weeklyVapes: {
+      id: number;
+      userId: number;
+      duration: number;
+      date: Date;
+    }[],
+  ) {
+    const daysOfWeek: string[] = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+
+    const response = {
+      Sunday: 0,
+      Monday: 0,
+      Tuesday: 0,
+      Wednesday: 0,
+      Thursday: 0,
+      Friday: 0,
+      Saturday: 0,
+    };
+
+    weeklyVapes.forEach((vape) => {
+      const vapeDate = vape.date;
+      const dayIndex = vapeDate.getDay();
+      const dayOfWeek = daysOfWeek[dayIndex];
+      response[dayOfWeek] += 1;
+    });
+
+    return response;
+  }
 }
