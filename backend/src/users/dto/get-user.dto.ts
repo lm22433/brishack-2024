0b71db496953fe06db484ef6d82f634d49cdcc51
Harry <lm22433@bristol.ac.uk>
2024-02-25 20:43:50 +0000

    link up user accounts

---
+++ backend/src/users/dto/get-user.dto.ts
@@ -0,0 +1,6 @@
+export class GetUserDto {
+  userId: string;
+  username: string;
+  email: string;
+  name: string;
+}
--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -9,24 +9,29 @@ function getRandomInt(min: number, max: number): number {
 }
 
 function Dashboard() {
-    const [streak, setStreak] = useState(5)
-    const [turkey, setTurkey] = useState(2)
-    let index = getRandomInt(0,8)
-    const [msg, setMsg] = useState(Messages[index])
+  const [streak, setStreak] = useState(5);
+  const [turkey, setTurkey] = useState(2);
+  let index = getRandomInt(0, 8);
+  const [msg, setMsg] = useState(Messages[index]);
 
-    return (
-        <>
-            <Header />
-            <SidebarButton />
-            <h1>Dashboard</h1>
-            <h2>Profile Username Here</h2>
-            <div><Timer initialTime={60} /></div>
-            <div>{msg}</div>
-            <div>
-                <a style={{textAlign: "center", margin: 50}}>Streak: {streak}</a>
-                <a style={{textAlign: "center", margin: 50}}>Largest Turkey: {turkey}</a>
-            </div>
-        </>
-    )
+  return (
+    <>
+      <Header />
+      <SidebarButton />
+      <h1>Dashboard</h1>
+      <h2>Username: {localStorage.getItem("username")}</h2>
+      <h2>Name: {localStorage.getItem("name")}</h2>
+      <div>
+        <Timer initialTime={60} />
+      </div>
+      <div>{msg}</div>
+      <div>
+        <a style={{ textAlign: "center", margin: 50 }}>Streak: {streak}</a>
+        <a style={{ textAlign: "center", margin: 50 }}>
+          Largest Turkey: {turkey}
+        </a>
+      </div>
+    </>
+  );
 }
-export default Dashboard
+export default Dashboard;
