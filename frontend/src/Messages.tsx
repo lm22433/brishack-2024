6a9b15237037a60c13606236eaa1447cbaf8c7c9
Josh <dw22682@bristol.ac.uk>
2024-02-25 18:25:05 +0000

    fixed spelling error

--- frontend/src/Messages.tsx
+++ frontend/src/Messages.tsx
@@ -1,6 +1,6 @@
 export const Messages = [
     "The greatest glory in living lies not in never falling, but in rising every time we fall - Nelson Mandela",
-    "Don't let the pas steal the present - Terri Guillemets",
+    "Don't let the past steal the present - Terri Guillemets",
     "Recovery is hard. Regret is harder - Brittany Burgunder",
     "The only way out is through - Robert Frost",
     "You are never too old to set another goal or to dream a new dream - C.S.Lewis",
