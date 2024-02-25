efcf87c4a455529b8c4b159f4aea41ebd1c1d50e
Josh <dw22682@bristol.ac.uk>
2024-02-25 18:16:34 +0000

    added random motivational messages

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import { Messages } from "./Messages";
 
 function getRandomInt(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -7,13 +8,15 @@ function getRandomInt(min: number, max: number): number {
 function Dashboard() {
     const [streak, setStreak] = useState(5)
     const [turkey, setTurkey] = useState(2)
-  
+    let index = getRandomInt(0,5)
+    const [msg, setMsg] = useState(Messages[index])
+
     return (
       <>
         <h1>Dashboard</h1>
         <h2>Profile Username Here</h2>
         <div>Image and Timer Here</div>
-        <div>Motivational Message</div>
+        <div>{msg}</div>
         <div>
             <a style={{textAlign: "center", margin: 50}}>Streak: {streak}</a>
             <a style={{textAlign: "center", margin: 50}}>Largest Turkey: {turkey}</a>
---
+++ frontend/src/Messages.tsx
@@ -0,0 +1,8 @@
+export const Messages = [
+    "The greatest glory in living lies not in never falling, but in rising every time we fall - Nelson Mandela",
+    "Don't let the pas steal the present - Terri Guillemets",
+    "Recovery is hard. Regret is harder - Brittany Burgunder",
+    "The only way out is through - Robert Frost",
+    "You are never too old to set another goal or to dream a new dream - C.S.Lewis",
+    "Every adversity, every failure, every heartache carries with it the seed of an equal or greater benefit - Napoleon Hill"
+]
