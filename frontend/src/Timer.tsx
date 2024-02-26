274d8edc9b187b6302a54979da3ba14e5a66c71d
Josh <dw22682@bristol.ac.uk>
2024-02-26 03:31:49 +0000

    added confetti when the timer reaches 0

--- frontend/src/Timer.tsx
+++ frontend/src/Timer.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import Confetti from "react-confetti"
 
 type TimerProps = {
   initialTime: number;
@@ -9,7 +10,15 @@ function Timer(inp: TimerProps) {
     const [time, setTime] = useState(inp.initialTime);
     const [isRunning, setIsRunning] = useState(true);
     const [newTime, setNewTime] = useState('');
-        
+    const [timerReachedZero, setTimerReachedZero] = useState(false);
+
+    useEffect(() => {
+      if (time === 0) {
+        setTimerReachedZero(true)
+        setIsRunning(false)
+      }
+    }, [time]);
+
     useEffect(() => {
       let interval: number | undefined;
       if (isRunning) {
@@ -27,6 +36,7 @@ function Timer(inp: TimerProps) {
     const handleReset = () => {
       setTime(parseInt(newTime, 10));
       setNewTime('')
+      setTimerReachedZero(false)
     }
         
     const formatTime = (time: number) => {
@@ -37,6 +47,8 @@ function Timer(inp: TimerProps) {
         
     return (
         <div>
+          {timerReachedZero ? (
+        <Confetti width={window.innerWidth} height={window.innerHeight} />) : null}
             <h1>Timer: {formatTime(time)}</h1>
             <button onClick={handlePausePlay}>{isRunning ? 'Pause' : 'Play'}</button>
             <input
