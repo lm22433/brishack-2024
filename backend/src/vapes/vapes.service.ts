0f552b4fd24ec61f6185c4c416ef038f072f1911
Harry <lm22433@bristol.ac.uk>
2024-02-26 00:08:57 +0000

    dynamic nic levels

--- backend/src/vapes/vapes.service.ts
+++ backend/src/vapes/vapes.service.ts
@@ -80,17 +80,7 @@ export class VapesService {
 
   async getClusteredDailyVapesByUserId(userId: string) {
     const weeklyVapes = await this.getWeeklyVapesByUserId(userId);
-    return this.clusterIntoDays(weeklyVapes);
-  }
 
-  clusterIntoDays(
-    weeklyVapes: {
-      id: number;
-      userId: number;
-      duration: number;
-      date: Date;
-    }[],
-  ) {
     const daysOfWeek: string[] = [
       'Sunday',
       'Monday',
@@ -116,6 +106,39 @@ export class VapesService {
       const dayIndex = vapeDate.getDay();
       const dayOfWeek = daysOfWeek[dayIndex];
       response[dayOfWeek] += 1;
+    });
+
+    return response;
+  }
+
+  async getClusteredDailyVapeDurationsByUserId(userId: string) {
+    const weeklyVapes = await this.getWeeklyVapesByUserId(userId);
+
+    const daysOfWeek: string[] = [
+      'Sunday',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+    ];
+
+    const response = {
+      Sunday: 0,
+      Monday: 0,
+      Tuesday: 0,
+      Wednesday: 0,
+      Thursday: 0,
+      Friday: 0,
+      Saturday: 0,
+    };
+
+    weeklyVapes.forEach((vape) => {
+      const vapeDate = vape.date;
+      const dayIndex = vapeDate.getDay();
+      const dayOfWeek = daysOfWeek[dayIndex];
+      response[dayOfWeek] += vape.duration;
     });
 
     return response;
