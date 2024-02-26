11c916244b458c95761aa314ea43e68c680ebb9d
Josh <dw22682@bristol.ac.uk>
2024-02-26 03:22:56 +0000

    improved timer class

--- frontend/src/Timer.tsx
+++ frontend/src/Timer.tsx
@@ -6,22 +6,28 @@ type TimerProps = {
 
 function Timer(inp: TimerProps) {
 
-        const [time, setTime] = useState(inp.initialTime);
-        const [isRunning, setIsRunning] = useState(true);
+    const [time, setTime] = useState(inp.initialTime);
+    const [isRunning, setIsRunning] = useState(true);
+    const [newTime, setNewTime] = useState('');
         
-        useEffect(() => {
-            let interval: number | undefined;
-            if (isRunning) {
-              interval = setInterval(() => {
-                setTime((prevTime: number) => (prevTime > 0 ? prevTime - 1 : 0));
-              }, 1000);
-            }
-            return () => clearInterval(interval);
-          }, [isRunning]);
-        
-          const handlePausePlay = () => {
-            setIsRunning(prevIsRunning => !prevIsRunning);
-          };
+    useEffect(() => {
+      let interval: number | undefined;
+      if (isRunning) {
+        interval = setInterval(() => {
+        setTime((prevTime: number) => (prevTime > 0 ? prevTime - 1 : 0));
+        }, 1000);
+      }
+      return () => clearInterval(interval);
+    }, [isRunning]);
+      
+    const handlePausePlay = () => {
+      setIsRunning(prevIsRunning => !prevIsRunning);
+    };
+
+    const handleReset = () => {
+      setTime(parseInt(newTime, 10));
+      setNewTime('')
+    }
         
     const formatTime = (time: number) => {
         const minutes = Math.floor(time / 60);
@@ -33,6 +39,12 @@ function Timer(inp: TimerProps) {
         <div>
             <h1>Timer: {formatTime(time)}</h1>
             <button onClick={handlePausePlay}>{isRunning ? 'Pause' : 'Play'}</button>
+            <input
+              type="number"
+              value={newTime}
+              onChange={(e) => setNewTime(e.target.value)}
+              placeholder="Set New Timer" />
+            <button onClick={handleReset}>Reset</button>
         </div>
     );
 };
